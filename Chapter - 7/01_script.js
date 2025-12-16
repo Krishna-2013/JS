2d863cafc9b01e2def801be8542e8652ed58d3dd
Krishna-2013 <anhsirksettings@gmail.com>
2025-12-16 22:17:45 +0600

    DOM part - 1

---
+++ Chapter - 7/01_script.js
@@ -0,0 +1,11 @@
+let firstel = document.querySelector("#b1");
+
+console.log(firstel);
+console.log(firstel.tagName);
+console.log(document.body.firstChild);
+console.log(document.body.lastChild);
+
+
+let div = document.querySelector("#d2");
+console.log(div.innerHTML);
+console.log(div.innerText);
